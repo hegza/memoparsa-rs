d8c7149748688a98f6b45edad335e91a68c8a3b0
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2019-11-30 19:21:06 +0200

    First working functionality with happy path

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,80 @@
 #[macro_use]
 extern crate clap;
 extern crate exitcode;
+use chrono::prelude::*;
 use clap::{App, Arg};
 
 static OUTPUT: &str = "parsa.ics";
 
 fn main() {
+    let matches = cli();
+
+    let source_contents = std::fs::read_to_string(matches.value_of("input").unwrap()).unwrap();
+    let format = match matches.value_of("format").unwrap().as_ref() {
+        "alpha" | "ALPHA" => memoparsa::SourceFormat::Alpha,
+        _ => panic!("unknown format"),
+    };
+    let year = matches.value_of("start-year");
+    let output_file = matches.value_of("output").unwrap();
+
+    // do work
+    memoparsa::save_as_ics(
+        format,
+        year.unwrap().parse::<i32>().unwrap(),
+        &source_contents,
+        output_file,
+    );
+
+    std::process::exit(exitcode::OK);
+}
+
+fn cli<'a>() -> clap::ArgMatches<'a> {
     let matches = App::new("memoparsa")
         .version(crate_version!())
         .author(crate_authors!("\n"))
         .about(crate_description!())
+        .arg(Arg::from_usage("-o, --output=[FILE] 'Sets custom output file'").default_value(OUTPUT))
         .args_from_usage(
-            "-o, --output=[FILE] 'Sets custom output file'
-            <INPUT>              'Sets input file to use'",
+            "<input>              'Sets input file to use'",
         )
         .arg(
-            Arg::from_usage("<FORMAT>             'Sets input format'")
+            Arg::from_usage("<format>             'Sets input format'")
                 .validator(validate_input_format_spec),
+        )
+        .arg(
+            Arg::from_usage("-y, --start-year=[NUMBER] 'Sets custom start year as context for the input file. This year is used by default for formats that require it.'")
+                .default_value_if("format", Some("alpha"), &Local::now().year().to_string())
         )
         .get_matches();
     println!("Tester program for cli implementation");
+    if let Some(year) = matches.value_of("start-year") {
+        println!("Selected start-year: {}", year);
+    } else {
+        let year = Local::now().year();
+        println!(
+            "Using default year, current year in local time-zone: {}",
+            year
+        );
+    }
+    if let Some(input) = matches.value_of("input") {
+        println!("Selected input file: {}", input);
+    } else {
+        println! {"Fatal error: no input file specified"};
+        std::process::exit(exitcode::DATAERR);
+    }
+    if let Some(in_format) = matches.value_of("format") {
+        println!("Selected input format: {}", in_format);
+    } else {
+        println! {"Fatal error: no input format specified"};
+        std::process::exit(exitcode::DATAERR);
+    }
     if let Some(output) = matches.value_of("output") {
         println!("Selected output file: {}", output);
     } else {
         let output = OUTPUT;
         println!("Using default output file: {}", output);
     }
-    if let Some(input) = matches.value_of("INPUT") {
-        println!("Selected input file: {}", input);
-    } else {
-        println! {"Fatal error: no input file specified"};
-        std::process::exit(exitcode::DATAERR);
-    }
-    if let Some(in_format) = matches.value_of("FORMAT") {
-        println!("Selected input format: {}", in_format);
-    } else {
-        println! {"Fatal error: no input format specified"};
-        std::process::exit(exitcode::DATAERR);
-    }
-    std::process::exit(exitcode::OK);
+    matches
 }
 
 /** Checks that the input format specifier is one of the allowed formats, eg.
